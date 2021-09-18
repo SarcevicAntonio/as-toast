4a2dba55b7504e89aab7a3644522528f96cccf78
Antonio Sarcevic <sarcevic@westphalia-datalab.com>
2021-09-19 00:50:10 +0200

    docs path

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,9 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
     adapter: staticAdapter(),
+    paths: {
+      base: '/as-toast'
+    }
   },
 };
 
