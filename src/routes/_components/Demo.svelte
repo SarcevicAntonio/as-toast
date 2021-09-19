caf01a722660cc975168578d698b58182900f2c9
Antonio Sarcevic <sarcevic@westphalia-datalab.com>
2021-09-19 02:08:33 +0200

    docs: button shadow

--- src/routes/_components/Demo.svelte
+++ src/routes/_components/Demo.svelte
@@ -18,7 +18,6 @@
 >
 <br />
 
-
 <button
   on:click={() => {
     addToast("Just a simple 'info' toast", "info", 999999999);
@@ -42,5 +41,12 @@
     min-height: 48px;
     margin: 1em 0;
     cursor: pointer;
+    box-shadow: none;
+    transition: box-shadow 0.4s ease-in-out;
+  }
+  button:hover {
+    box-shadow: 0 0.3px 1.4px rgba(0, 0, 0, 0.068), 0 0.7px 3.5px rgba(0, 0, 0, 0.098),
+      0 1.4px 7.1px rgba(0, 0, 0, 0.122), 0 2.9px 14.6px rgba(0, 0, 0, 0.152),
+      0 8px 40px rgba(0, 0, 0, 0.22);
   }
 </style>
